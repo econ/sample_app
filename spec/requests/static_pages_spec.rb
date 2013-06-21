ef8cab59d72bf5cbb3630f7f56e9baa3086e7150
econ <ashby1985@hotmail.com>
2013-06-21 09:32:52 -0400

    Passed some tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | Help")
+      page.should have_selector('title', :text => "Help")
     end
   end
 
@@ -43,20 +43,21 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | About Us")
+      page.should have_selector('title', :text => "About Us")
     end
   end
 
-  describe "Contact" do
+  describe "Contact page" do
 
-    it "should have the content 'Contact'" do
+    it "should have the h1 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => "#{base_title} | Contact")
+      page.should have_selector('title', 
+                      text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
